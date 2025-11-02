88c3c16a2153b5d4a45b2410625dd6b11889f24e
visith kumarapperuma <visithr9@gmail.com>
2025-11-02 18:47:03 +0530

    Initial Commit

---
+++ MovieMaze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieMaze
+//
+//  Created by visith kumarapperuma on 2025-11-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieMaze/MovieMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMazeApp.swift
+//  MovieMaze
+//
+//  Created by visith kumarapperuma on 2025-11-02.
+//
+
+import SwiftUI
+
+@main
+struct MovieMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
